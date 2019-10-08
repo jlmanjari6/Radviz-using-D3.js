6473e49313fd8b094639a337b6e99ce86d322a39
Manjari <lk659879@dal.ca>
2019-10-07 23:01:39 -0300

    tool tip text changed

--- app.js
+++ app.js
@@ -3,32 +3,34 @@ var CIRCLE_RADIUS = 250;
 var CIRCLE_CENTERX = 300;
 var CIRCLE_CENTERY = 300;
 var CLASS_NAME = "";
-var DATASET = "iris.csv"
+var DATASET = "winequality-red.csv"
 var DATA = Object;
+var arrOriginalNormalizedVal = [];
 
-generatePlot(DATASET);
+document.addEventListener("DOMContentLoaded", function() {    
+    d3.csv(DATASET).then(function(data) {
+        generatePlot(data); // assigning default dataset for initial upload
+    });
+});
 
-// on change of dataset selection from dropdown
-function onDataSetChange() {
-    dataset_selected = document.getElementById("csvSelector").selectedIndex;
-    if(dataset_selected == 0){
-        DATASET = "iris.csv"
-    }
-    else if(dataset_selected == 1) {
-        DATASET = "winequality-red.csv";
-    }
-    else if(dataset_selected == 2) {
-        DATASET = "winequality-white.csv";
-    }
-
-    d3.select("svg").remove();
-    this.generatePlot(DATASET);
+// on choosing file to upload from file upload dialog box
+function onFileUploadBtnClick() {
+    var file = document.getElementById("fileUpload").files[0];
+    if (file) {
+        var reader = new FileReader();
+        reader.readAsText(file, "UTF-8");        
+        reader.onload = function (e) {
+            DATA = d3.csvParse(e.target.result);
+            d3.select("svg").remove();
+            generatePlot(DATA);            
+        }
+    }         
 }
 
-//display anchors checkboxes
+// display anchors checkboxes
 function displayanchors(columns) {
     // remove existing checkboxes
-    d3.selectAll("input").remove();
+    d3.selectAll(".checkboxes").remove();
     d3.selectAll("text").remove();
 
     // add checkbox
@@ -41,6 +43,7 @@ function displayanchors(columns) {
         .attr("type", "checkbox")
         .attr("id", id)
         .attr("name", "checkboxes")
+        .attr("class", "checkboxes")
         .attr("onchange", "onAnchorSetChange()")
         .attr("checked", true)  
         .style("zoom", "2.0");
@@ -75,16 +78,14 @@ function onAnchorSetChange(){
     else {
         d3.select("#warningmessage").remove(); //removing existing warning messages
         d3.selectAll(".plotcircles").remove(); //removing existing plot anchors and data points
-        anchor_positions = this.plotAnchors(columnList); //to plot new anchors
+        anchor_positions = this.plotAnchors(columnList, false); //to plot new anchors
         this.plotInstances(DATA, anchor_positions); //to plot data points by anchor positions
-    }
-    console.log(columnList);
+    }    
 }
 
 // generate Radviz
-function generatePlot(DATASET) {
-    // load csv
-    d3.csv(DATASET).then(function(data) {
+function generatePlot(data) {
+    // load csv   
         DATA = data;
         columns = data.columns;
 
@@ -93,9 +94,7 @@ function generatePlot(DATASET) {
             anchors.push(columns[i]);
         }
 
-        displayanchors(anchors);
-
-        console.log("data: " , data);    
+        displayanchors(anchors);         
 
         // parse and convert strings to numbers except last column
         data.forEach(function(vector){    
@@ -111,22 +110,28 @@ function generatePlot(DATASET) {
         }); 
 
         // normalize all columns except last column to a range of 0 and 1
-        dict_scalefunction = {};
+        arrOriginalNormalizedVal = [];
+        dictScaleFunction = {}; //dic of column name, scale function
         for(i=0; i<columns.length-1; i++) {    
             max = d3.max(data, function(d) { return d[columns[i]]; });
             min = d3.min(data, function(d) { return d[columns[i]]; });
             func = d3.scaleLinear().domain([min,max]);
-            dict_scalefunction[columns[i]] = func;
+            dictScaleFunction[columns[i]] = func;
         }
-        data.forEach(function(vector){    
+        row = 0;
+        data.forEach(function(vector){    //replacing original values with normalized values
+            arrVectorTemp = [];
+            i = 0;
             for(key in vector){    
-                if(key != CLASS_NAME) {    
-                    vector[key] = dict_scalefunction[key](vector[key]);  
+                arrVectorTemp[i] = vector[key];
+                if(key != CLASS_NAME) {                                                              
+                    vector[key] = dictScaleFunction[key](vector[key]); //passing each cell value to scale function                    
+                    i++;
                 }
-            }        
-        }); 
-
-        console.log("converted data: " , data);
+            }
+            arrOriginalNormalizedVal[row] = arrVectorTemp; 
+            row++;       
+        });         
 
         // arrange svg canvas
         svg_container = d3.select("#svgcontainer")
@@ -142,47 +147,60 @@ function generatePlot(DATASET) {
                     .attr("fill", "white")
                     .attr("stroke", "black");
 
-        anchor_positions = this.plotAnchors(anchors);
+        anchor_positions = this.plotAnchors(anchors, false);
         this.plotInstances(data, anchor_positions);
         this.addToolTip();
-    });
+    
 }
 
 // plot anchors on circle
-plotAnchors = function(columns) {
-    anchors_count = columns.length;
+plotAnchors = function(columns, isDragged) {
+    if(!isDragged) {
+        anchors_count = columns.length;
+        var anchors_positions = {};
 
-    var anchors_positions = {};
+        for(i=0; i<anchors_count; i++) {
+            anchor_posX = (this.CIRCLE_RADIUS 
+                            * Math.cos((i * (360/anchors_count)) 
+                            * (parseFloat(Math.PI/180)))) 
+                                + this.CIRCLE_CENTERX;
 
-    for(i=0; i<anchors_count; i++) {
-        anchor_posX = (this.CIRCLE_RADIUS * Math.cos(i * (360/anchors_count))) 
-                            + this.CIRCLE_CENTERX;
+            anchor_posY = (this.CIRCLE_RADIUS 
+                            * Math.sin((i * (360/anchors_count)) 
+                            * (parseFloat(Math.PI/180)))) 
+                                + this.CIRCLE_CENTERY;
 
-        anchor_posY = (this.CIRCLE_RADIUS * Math.sin(i * (360/anchors_count))) 
-                            + this.CIRCLE_CENTERY;
+            // saving anchors along with their positions in dictionary                    
+            positions = [anchor_posX, anchor_posY];
+            anchor_name = columns[i];
+            anchors_positions[anchor_name] = positions;
+            
+            // draw anchors on circle
+            svg_container = d3.select("svg")
+            circle = svg_container.append("circle")
+                                    .attr("cx", anchor_posX)
+                                    .attr("cy", anchor_posY)
+                                    .attr("r", 8)
+                                    .attr("class", "plotcircles")
+                                    .attr("fill", "red")
+                                    .call(d3.drag()
+                                    .on('start', dragstarted)
+                                    .on('drag', dragged)
+                                    .on('end', dragended)
+                                );
+            
+            // add text for each anchor                        
+            svg_container.append("text")
+                        .attr("x", anchor_posX + 10)
+                        .attr("y", anchor_posY + 10)
+                        .attr("class", "plotcircles")
+                        .text(columns[i]);
 
-        // saving anchors along with their positions in dictionary                    
-        positions = [anchor_posX, anchor_posY];
-        anchor_name = columns[i];
-        anchors_positions[anchor_name] = positions;
-        
-        // draw anchors on circle
-        svg_container = d3.select("svg")
-        circle = svg_container.append("circle")
-                                .attr("cx", anchor_posX)
-                                .attr("cy", anchor_posY)
-                                .attr("r", 5)
-                                .attr("class", "plotcircles")
-                                .attr("fill", "red");
-        
-        // add text for each anchor                        
-        svg_container.append("text")
-                    .attr("x", anchor_posX + 6)
-                    .attr("y", anchor_posY + 6)
-                    .attr("class", "plotcircles")
-                    .text(columns[i]);
+        }
+    }
+    else {
 
-      }
+    }
       return anchors_positions;
 }
 
@@ -198,6 +216,7 @@ this.plotInstances = function(data, anchor_positions) {
     }
 
     // loop through each row and plot each instance with color based on class
+    row = 0;
     data.forEach(function(vector){
         sum_upper_X = 0;
         sum_upper_Y = 0;
@@ -230,16 +249,63 @@ this.plotInstances = function(data, anchor_positions) {
                     
         // add tool tip to all data plots                                  
         circle.append("svg:title")
-             .text(this.addToolTip(vector));
-    });
+             .text(this.addToolTip(vector, row));
+        row++;
+    });    
 }
 
 // add tool tip for each instance
-this.addToolTip = function(vector) {
-   text = "Normalized values to range [0,1]: \n\n";
+this.addToolTip = function(vector, row) {
+   text = "";
+   arrTemp = arrOriginalNormalizedVal[row];
+   i = 0;
    for(key in vector) {
-       text += key + ": " + vector[key] + "\n";
+       if(i == arrTemp.length-1) {
+           text += "\n";
+       }
+       text += key + ": " + arrTemp[i] + "\n";       
+       i++;
    }
+   
    return text;
 }
 
+// dragstarted, dragged, dragended
+function dragstarted(d){ 
+    d3.select(this).raise().classed('active', true);
+    console.log("drag started");
+    
+}
+function dragended(d){ 
+    d3.select(this).classed('active', false);
+    d3.select(this).attr('stroke-width', 0);
+    console.log("drag ended");
+}
+function dragged(d, i) {
+    d3.select(this).raise().classed('active', true);
+    console.log("dragged");
+   
+    cursorX = d3.event.x;
+    cursorY = d3.event.y;
+    distance = Math.sqrt(Math.pow((cursorX-CIRCLE_CENTERX),2) 
+                        + Math.pow((cursorY-CIRCLE_CENTERY) ,2));
+
+                        console.log(distance, ", ", CIRCLE_RADIUS);
+    if(Math.abs(distance - CIRCLE_RADIUS) <= 3) {                        
+    // redraw the dimensional anchor and the label
+    d3.selectAll(".plotcircles").remove();                        
+    svg_container = d3.select("svg")
+    circle = svg_container.append("circle")
+                            .attr("cx", d3.event.x )
+                            .attr("cy", d3.event.y )
+                            .attr("r", 8)
+                            .attr("class", "plotcircles")
+                            .attr("fill", "red")
+                            .call(d3.drag()
+                            .on('start', dragstarted)
+                            .on('drag', dragged)
+                            .on('end', dragended)
+                        );
+                        
+    }    
+}
